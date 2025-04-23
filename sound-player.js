0152491e022eea07c30529c210d33c625926e4c8
AlexNovickiy <127426566+AlexNovickiy@users.noreply.github.com>
2025-04-23 12:58:59 +0000

    Deploying to gh-pages from @ AlexNovickiy/js-tasks-practice@79a3da86e7fce752babdd7ff4309242cf8b2ed3d 🚀

--- sound-player.js
+++ sound-player.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const o=document.querySelectorAll(".sound-button");o.forEach(e=>{e.addEventListener("click",()=>{const d=e.getAttribute("data-sound");new Audio(`/audio/${d}.mp3`).play(),e.classList.add("clicked"),setTimeout(()=>{e.classList.remove("clicked")},200)})});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const t=document.querySelectorAll(".sound-button");t.forEach(e=>{e.addEventListener("click",()=>{const s=e.getAttribute("data-sound");new Audio(`/${s}.mp3`).play(),e.classList.add("clicked"),setTimeout(()=>{e.classList.remove("clicked")},200)})});
 //# sourceMappingURL=sound-player.js.map
